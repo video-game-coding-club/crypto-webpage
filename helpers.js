463ca816430663514bf5f1b59cec4f091ecd1e86
zmedin <zmedin@gmail.com>
2020-10-30 18:37:36 +0000

    Fixes video-game-coding-club/crypto-webpage#79
    Signed-off-by: zmedin <zmedin@gmail.com>

--- helpers.js
+++ helpers.js
@@ -28,8 +28,40 @@ window.processForm = processForm;
  * letter.
  */
 export function shiftLetter (letter, shift) {
-  var code = letter.charCodeAt(0) + shift;
+  // First change the letter into a number.
+  var code = letter.charCodeAt(0);
+  var Acode = 'A'.charCodeAt(0);
+  var Zcode = 'Z'.charCodeAt(0);
+  var acode = 'a'.charCodeAt(0);
+  var zcode = 'z'.charCodeAt(0);
+
+  // Only shift if the letter really is a letter.
+  if (code >= Acode && code <= Zcode) {
+    // The letter is a capital letter. Shift it.  
+    code += shift;
+    // If code is too big or too small, wrap it so it isn't.
+    code = wrapNumber(code,Acode,Zcode);
+  } else if (code >= acode && code <= zcode) {
+    // The letter is a lowercase letter. Shift it and wrap it.
+    code += shift;
+    code = wrapNumber(code,acode,zcode);
+  }
+
   return String.fromCharCode(code);
+}
+
+/* Takes a number and wraps it around so it fits inside the interval.
+ */
+function wrapNumber (code, low, high) {
+  var wrap = high - low + 1;
+
+  if (code > high) {
+    code -= wrap;
+  } else if (code < low) {
+    code += wrap;
+  }
+
+  return code;
 }
 
 /* Initialize language setting on page load
